48178fb88457afcc894d7244ad2901f785db5c42
Luiz Henrique <126366367+luizhenriquesb@users.noreply.github.com>
2024-01-18 12:07:55 -0300

    Update grafico_bpc.R

--- grafico_bpc.R
+++ grafico_bpc.R
@@ -24,46 +24,67 @@ dados <- df |>
     data = as.Date(paste(ano_competencia, mes_competencia, "01", sep = "-"))
   )
 
-dados |> 
+dados |>
   ggplot() +
   aes(x = data, y = perc) +
-  geom_line(colour = "#1351B4") +
+  geom_line(colour = "#1351B4", size = 1.25) +
   theme_classic() +
   ggrepel::geom_label_repel(
     aes(label = qtde_beneficiarios),
-    max.overlaps = 4 # quanto mais, mais labels
+    max.overlaps = 3,
+    fill = "#1351B4",
+    colour = "grey100"
   ) +
-  scale_x_date(date_labels = "%b/%y",
-               date_breaks = "2 months") +
+  scale_x_date(
+    date_labels = "%b/%y",
+    date_breaks = "3 months"
+  ) +
   scale_y_continuous(labels = scales::percent) +
   labs(
     title = "Beneficío de Prestação Continuada",
-    subtitle = "Percentual de beneficiários em relação à população total de São Paulo (SP)",
+    subtitle = "Percentual de beneficiários do município de São Paulo (SP) em relação à população total da cidade",
     caption = "Fonte: Base dos Dados | Controladoria Geral da União | IBGE"
   ) +
   theme(
     plot.title = element_text(
       colour = "#1351B4",
-      face = "bold"),
+      face = "bold",
+      size = 14
+    ),
     plot.subtitle = element_text(
-      colour = "grey10"
+      colour = "grey10",
+      size = 12
     ),
     plot.caption = element_text(
-      colour = "grey20"
+      colour = "grey20",
+      size = 10,
+      vjust = 0.01
     ),
-    axis.text = element_text(colour = "black", face = "bold"),
-    axis.text.x = element_text(angle = 45, hjust = 1.25),
-    panel.grid.major.y = element_line(color = "grey60", 
-                                      linetype = "dashed",
-                                      linewidth = .35),
+    axis.text = element_text(colour = "grey20", face = "bold"),
+    axis.text.x = element_text(angle = 30, hjust = 1),
+    axis.text.y = element_text(size = 11),
+    panel.grid.major.y = element_line(
+      color = "grey60",
+      linetype = "dashed",
+      linewidth = .35
+    ),
     axis.title = element_blank(),
     axis.ticks.x = element_line(colour = "black"),
     axis.ticks.y = element_blank(),
     axis.line.x = element_line(colour = "black", linewidth = .75),
     axis.line.y = element_blank(),
     panel.background = element_rect(
-      fill = "grey90"
+      fill = "grey100"
     ),
     plot.background = element_rect(
-      fill = "grey90"
-    ))
+      fill = "grey100"
+    )
+  ) -> plot
+
+ggsave("grafico_bpc.jpeg",
+       plot = plot,
+       path = "cgu/",
+       width = 12,
+       height = 6,
+       dpi = 1000
+       )
